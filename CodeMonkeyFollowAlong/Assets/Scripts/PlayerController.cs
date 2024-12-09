8989d81ee527e2e6e3827d0cf57d328488beef0b
Matthew Camacho <mcamacho2056@gmail.com>
2024-12-09 14:03:47 -0700

    tied the movement to the input of the player

--- CodeMonkeyFollowAlong/Assets/Scripts/PlayerController.cs
+++ CodeMonkeyFollowAlong/Assets/Scripts/PlayerController.cs
@@ -6,21 +6,26 @@ public class PlayerController : MonoBehaviour
 {
     private void Update()
     {
-       if(Input.GetKey(KeyCode.W))
-       {
+        Vector3 inputVector = new Vector3(0, 0, 0);
+        if(Input.GetKey(KeyCode.W))
+        {
+             inputVector.z = +1;
+        }
+        if(Input.GetKey(KeyCode.S))
+        {
+             inputVector.z = -1; 
+        }
+        if (Input.GetKey(KeyCode.D)) 
+        {
+             inputVector.x = +1;
+        }
+        if( Input.GetKey(KeyCode.A))
+        {
+              inputVector.x = -1;
+        }
 
-       }
-       if(Input.GetKey(KeyCode.S))
-       {
+        inputVector = inputVector.normalized;
 
-       }
-       if (Input.GetKey(KeyCode.D)) 
-       {
-        
-       }
-       if( Input.GetKey(KeyCode.A))
-       {
-
-       }
+        transform.position += inputVector;
     }
 }
