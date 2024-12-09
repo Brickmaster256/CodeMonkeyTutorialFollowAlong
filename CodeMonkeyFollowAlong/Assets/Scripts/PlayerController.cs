15b4c9b222411cdb8764be871c152bb6e087fcb7
Matthew Camacho <mcamacho2056@gmail.com>
2024-12-09 13:52:55 -0700

    created a character and the frameworks of a script to move the character

---
+++ CodeMonkeyFollowAlong/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private void Update()
+    {
+       if(Input.GetKey(KeyCode.W))
+       {
+
+       }
+       if(Input.GetKey(KeyCode.S))
+       {
+
+       }
+       if (Input.GetKey(KeyCode.D)) 
+       {
+        
+       }
+       if( Input.GetKey(KeyCode.A))
+       {
+
+       }
+    }
+}
