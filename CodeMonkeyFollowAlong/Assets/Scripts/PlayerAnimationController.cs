6c40790fbac42f0cd9a7ef283546e4e367abe0ab
Matthew Camacho <mcamacho2056@gmail.com>
2024-12-10 12:01:28 -0700

    change the animator to the premade one as it is slightly better, the one I made was for practice

--- CodeMonkeyFollowAlong/Assets/Scripts/PlayerAnimationController.cs
+++ CodeMonkeyFollowAlong/Assets/Scripts/PlayerAnimationController.cs
@@ -13,11 +13,11 @@ public class PlayerAnimationController : MonoBehaviour
     private void Awake()
     {
         animator = GetComponent<Animator>();
-        animator.SetBool(IS_WALKING, !player.IsWalking());
+        animator.SetBool(IS_WALKING, player.IsWalking());
     }
 
     private void Update()
     {
-        animator.SetBool(IS_WALKING, !player.IsWalking());
+        animator.SetBool(IS_WALKING, player.IsWalking());
     }
 }
