50ae231690d177ae7ea3135b704ddc8bb6fda808
Matthew Camacho <mcamacho2056@gmail.com>
2025-01-09 20:11:31 -0700

    created a location for where the tomato spawns and instantiated it on interaction

--- CodeMonkeyFollowAlong/Assets/Scripts/CounterClasses/ClearCounter.cs
+++ CodeMonkeyFollowAlong/Assets/Scripts/CounterClasses/ClearCounter.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class ClearCounter : MonoBehaviour
 {
     [SerializeField] private Transform tomatoPrefab;
-
+    [SerializeField] private Transform counterTopPoint;
 
     public void Interact()
     {
         Debug.Log("Interact");
+        Transform tomatoTransform = Instantiate(tomatoPrefab, counterTopPoint);
+        tomatoTransform.localPosition = Vector3.zero;
     }
 }
