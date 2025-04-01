f2df1bb39ae05598bcde0ad2bcaafb3a8f4fe0bf
Matthew Camacho <mcamacho2056@gmail.com>
2025-04-01 09:52:09 -0600

    created a basic start for the container script

---
+++ CodeMonkeyFollowAlong/Assets/Scripts/CounterClasses/CountainerCounter.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CountainerCounter : MonoBehaviour, IKitchenObjectParent
+{
+    [SerializeField] private KitchenObjectSO kitchenObjectSO;
+    [SerializeField] private Transform counterTopPoint;
+
+
+
+    private KitchenObject kitchenObject;
+    public void Interact(PlayerController player)
+    {
+        if (kitchenObject == null)
+        {
+            //Creates the object on the counter
+            Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab, counterTopPoint);
+            kitchenObjectTransform.GetComponent<KitchenObject>().SetKitchenObjectParent(this);
+            
+        }
+        else
+        {
+            //Give the object to the player
+            kitchenObject.SetKitchenObjectParent(player);
+
+        }
+    }
+
+    public Transform GetKitchenObjectFollowTransform()
+    {
+        return counterTopPoint;
+    }
+
+    public void SetKitchenObject(KitchenObject kitchenObject)
+    {
+        this.kitchenObject = kitchenObject;
+    }
+
+    public KitchenObject GetKitchenObject()
+    {
+        return kitchenObject;
+    }
+
+    public void ClearKitchenObject()
+    {
+        kitchenObject = null;
+    }
+
+    public bool HasKitcheObject()
+    {
+        return kitchenObject != null;
+    }
+}
