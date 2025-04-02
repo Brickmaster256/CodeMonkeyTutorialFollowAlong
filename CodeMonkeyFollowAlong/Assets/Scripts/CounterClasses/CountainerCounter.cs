9d772f21c558bbe1dc6d7969d13377220308ea79
Matthew Camacho <mcamacho2056@gmail.com>
2025-04-02 08:57:44 -0600

    added code to animate the containercounter to open

--- CodeMonkeyFollowAlong/Assets/Scripts/CounterClasses/CountainerCounter.cs
+++ CodeMonkeyFollowAlong/Assets/Scripts/CounterClasses/CountainerCounter.cs
@@ -1,18 +1,21 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CountainerCounter : BaseCounter
 {
+    public event EventHandler OnPlayerGrabbedObject;
+
     [SerializeField] private KitchenObjectSO kitchenObjectSO;
     
     public override void Interact(PlayerController player)
     {
         
-            //Creates the object on the counter
-            Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab);
-            kitchenObjectTransform.GetComponent<KitchenObject>().SetKitchenObjectParent(player);
-            
+            //gives the player an object
+         Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab);
+         kitchenObjectTransform.GetComponent<KitchenObject>().SetKitchenObjectParent(player);
+        OnPlayerGrabbedObject?.Invoke(this, EventArgs.Empty);
         
         
     }
