a0d1c85d473b359e3598d4b33ff2f4dd19d4f947
Matthew Camacho <mcamacho2056@gmail.com>
2025-04-02 08:48:02 -0600

    refactored the code of the counters to be cleaner and give player items

--- CodeMonkeyFollowAlong/Assets/Scripts/CounterClasses/ClearCounter.cs
+++ CodeMonkeyFollowAlong/Assets/Scripts/CounterClasses/ClearCounter.cs
@@ -6,56 +6,14 @@ public class ClearCounter : BaseCounter, IKitchenObjectParent
 {
 
     [SerializeField] private KitchenObjectSO kitchenObjectSO;
-    [SerializeField] private Transform counterTopPoint;
     
-    
-
-    private KitchenObject kitchenObject;
 
     
 
     public override void Interact(PlayerController player)
     {
-        if (kitchenObject == null)
-        {
-            //Creates the object on the counter
-            Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab, counterTopPoint);
-            kitchenObjectTransform.GetComponent<KitchenObject>().SetKitchenObjectParent(this);
-            kitchenObjectTransform.localPosition = Vector3.zero;
-
-            kitchenObject = kitchenObjectTransform.GetComponent<KitchenObject>();
-            kitchenObject.SetKitchenObjectParent(this);
-        }
-        else
-        {
-            //Give the object to the player
-            kitchenObject.SetKitchenObjectParent(player);
-            
-        }
+        
     }
 
-    public Transform GetKitchenObjectFollowTransform()
-    {
-        return counterTopPoint;
-    }
-
-    public void SetKitchenObject(KitchenObject kitchenObject)
-    {
-        this.kitchenObject = kitchenObject;
-    }
-
-    public KitchenObject GetKitchenObject()
-    {
-           return kitchenObject;
-    }
-
-    public  void ClearKitchenObject()
-    {
-        kitchenObject = null;
-    }
-
-    public bool HasKitcheObject()
-    {
-        return kitchenObject != null;
-    }
+    
 }
--- CodeMonkeyFollowAlong/Assets/Scripts/CounterClasses/CountainerCounter.cs
+++ CodeMonkeyFollowAlong/Assets/Scripts/CounterClasses/CountainerCounter.cs
@@ -2,53 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CountainerCounter : BaseCounter, IKitchenObjectParent
+public class CountainerCounter : BaseCounter
 {
     [SerializeField] private KitchenObjectSO kitchenObjectSO;
-    [SerializeField] private Transform counterTopPoint;
-
-
-
-    private KitchenObject kitchenObject;
+    
     public override void Interact(PlayerController player)
     {
-        if (kitchenObject == null)
-        {
+        
             //Creates the object on the counter
-            Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab, counterTopPoint);
-            kitchenObjectTransform.GetComponent<KitchenObject>().SetKitchenObjectParent(this);
+            Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab);
+            kitchenObjectTransform.GetComponent<KitchenObject>().SetKitchenObjectParent(player);
             
-        }
-        else
-        {
-            //Give the object to the player
-            kitchenObject.SetKitchenObjectParent(player);
-
-        }
+        
+        
     }
 
-    public Transform GetKitchenObjectFollowTransform()
-    {
-        return counterTopPoint;
-    }
-
-    public void SetKitchenObject(KitchenObject kitchenObject)
-    {
-        this.kitchenObject = kitchenObject;
-    }
-
-    public KitchenObject GetKitchenObject()
-    {
-        return kitchenObject;
-    }
-
-    public void ClearKitchenObject()
-    {
-        kitchenObject = null;
-    }
-
-    public bool HasKitcheObject()
-    {
-        return kitchenObject != null;
-    }
+    
 }
